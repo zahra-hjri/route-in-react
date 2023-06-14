bc30bd00b6b4f6f52b39ea7489b5e1d72e126f42
Zahra Hojabri <zhojabri@ymail.com>
2023-06-14 09:30:27 +0330

    added new api

--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -1,4 +1,7 @@
 import React , {useState , useEffect} from 'react'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
 
 function Todo() {
     const [todolist , setTodoList] = useState([])
@@ -12,14 +15,19 @@ function Todo() {
       .then(response => response.json())
       .then(json => setTodoList(json))
       }, []);
+      
   return (
-    <div className='bg-slate-900 h-screen pt-28'>
-        {todolist.slice(0,6).map((todo)=>{
+    
+    <div className='bg-slate-900 h-screen p-10 md:p-12'>
+      <Header />
+       <div className='mt-20'>
+       {todolist.slice(0,6).map((todo)=>{
             return<div className='flex p-4 justify-center' key={todo.id}>
                <p className={`px-2 ${todo.completed ? 'text-red-700 line-through' : 'text-green-500'}`}>{todo.title}</p>
                <input type='checkbox' onChange={() => handleOnChangeInput(todo.id)} />
                </div>
         })}
+       </div>
    
     </div>
   )
