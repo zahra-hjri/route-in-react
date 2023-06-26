42910051ba4ae9295a87ceef9aa9c2fc201ca3fb
zahra <zhojabri@ymail.com>
2023-06-26 09:28:37 +0330

    change style and responsive

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -8,11 +8,11 @@ const Detail = () => {
     const {id} = useParams()
     const mainProduct = Data.find((item)=> item.id == id)
   return (
-    <div className='mainDetail h-full p-8 sm:p-10'>
+    <div className='mainDetail h-full lg:h-screen p-8 sm:p-10'>
       <Header />
      <div className='grid grid-cols-1 md:grid-cols-2 my-20 place-items-center'>
     
-        <img className='w-4/5 md:w-full lg:w-4/5 h-72 md:h-64 lg:h-72 rounded-xl' src={mainProduct.url}></img>
+        <img className='w-full xl:w-4/5 h-72 md:h-64 lg:h-72 rounded-xl' src={mainProduct.url}></img>
         
       <div className='flex flex-col p-10 text-white'> 
       <strong className='text-2xl bg-clip-content p-6 bg-green-800 border-4 border-green-800 border-dashed'>{mainProduct.name}</strong>
